fe9ed9ce9abc6f8f95be2e749da3cff77b48a392
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-06-08 16:44:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+cats = ["Milo", "Otis", "Garfield"];
+function cats(){
+  console.log(cats)
+}
+cats;
+
+function destructivelyAppendCat(name){
+  cats.push ('Ralph')
+}
+destructivelyAppendCat;
+
+function destructivelyPrependCat(name){
+  cats.unshift ('Bob')
+}
+destructivelyPrependCat;
+
+function destructivelyRemoveLastCat(){
+  cats.pop()
+}
+destructivelyRemoveLastCat;
+
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+destructivelyRemoveFirstCat;
+
+function appendCat(name) {
+  const newCats = cats.slice();
+  newCats.push(name);
+  return newCats;
+}
+function prependCat(name) {
+  const newCats = cats.slice();
+  newCats.unshift(name);
+  return newCats;
+}
+function removeFirstCat(name) {
+  const newCats = cats.slice();
+  newCats.shift(name);
+  return newCats;
+}
+function removeLastCat(name) {
+  const newCats = cats.slice();
+  newCats.pop(name);
+  return newCats;
+}
+
+
